ee9f2e809a698449d9928b44fcd8d794eaa6b1ef
Stefani Quiolla <stefiquio@gmail.com>
2025-09-08 17:16:53 -0300

    update form

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,15 @@
-// src/utils/api.js
 import axios from "axios";
 
-// Normalizo la base para que SIEMPRE termine con "/"
-// (sirve tanto si pones http://localhost:4000/api como http://localhost:4000/api/)
+// Normalizo la base para termine con "/"
 const BASE_URL = (import.meta.env.VITE_BASE_URL || "").replace(/\/?$/, "/");
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL, // ej: http://localhost:4000/api/
+  baseURL: BASE_URL, // http://localhost:4000/api/
 });
 
 // Obtener todos los productos
 export const getProducts = async () => {
-  const resp = await axiosInstance.get("products"); // -> .../api/products
+  const resp = await axiosInstance.get("products"); //  .../api/products
   return resp.data.products;
 };
 
@@ -28,19 +26,13 @@ export const postProduct = async (body) => {
 
 // Obtener producto por ID
 export const getProductById = async (id) => {
-  const resp = await axiosInstance.get(`products/${id}`); // -> .../api/products/:id
+  const resp = await axiosInstance.get(`products/${id}`); // .../api/products/:id
   return resp.data;
 };
 
-// Contacto (si está en tu API)
+// Contacto
 // export const postContact = async (body) => {
 //   const resp = await axiosInstance.post("contact", body);
-//   return resp.data;
-// };
-
-// Preferencia de MercadoPago (si está en tu API)
-// export const postPreferenceMP = async (body) => {
-//   const resp = await axiosInstance.post("checkout", body);
 //   return resp.data;
 // };
 
